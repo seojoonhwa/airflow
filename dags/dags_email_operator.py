9bc750634831a9db9a692f2bc3f1dff947415cc1
seojoonhwa <tt6450@naver.com>
2024-02-25 14:54:01 +0900

    email operator 업로드

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -6,7 +6,7 @@ from airflow.operators.email import EmailOperator
 with DAG(
     dag_id="dags_email_operator",
     schedule="0 8 1 * *",
-    start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 2, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     send_email_task = EmailOperator(
