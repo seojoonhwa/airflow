32e1e773c1f908b0e13ec757d3403b33327a3368
seojoonhwa <tt6450@naver.com>
2024-03-01 10:23:12 +0900

    task operator 파일 업로드

---
+++ dags/dags_python_task_decorator.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+
+with DAG(
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=["example"],
+) as dag : 
+    
+    @task(task_id="python_task_1")
+    def print_context(some_input):
+        print(some_input)
+    
+    python_task_1 = print_context('task_decorator 실행')
+
+    # task decorator를 사용하면 객체를 나열할 필요가 없다
